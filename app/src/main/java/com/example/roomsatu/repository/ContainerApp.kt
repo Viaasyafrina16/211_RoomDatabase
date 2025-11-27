0709cd9f4563d439013bd0391d3b9cf97c7e288f
Viaasyafrina16 <syafrinavia@gmail.com>
2025-11-27 09:46:27 +0700

    membuat  interface RepositoriSiswa

---
+++ app/src/main/java/com/example/roomsatu/repository/ContainerApp.kt
@@ -0,0 +1,19 @@
+package com.example.roomsatu.repository
+
+
+import com.example.roomsatu.repositori.SiswaDao
+import com.example.roomsatu.room.Siswa
+import kotlinx.coroutines.flow.Flow
+
+interface RepositoriSiswa {
+    fun getAllSiswaStream(): Flow<List<Siswa>>
+
+    suspend fun insertSiswa(siswa: Siswa)
+}
+
+class OfflineRepositoriSiswa(
+    private val siswaDao: SiswaDao
+): RepositoriSiswa {
+    override fun getAllSiswaStream(): Flow<List<Siswa>> = siswaDao.getAllSiswa()
+    override suspend fun insertSiswa(siswa: Siswa) = siswaDao.insert(siswa)
+}
